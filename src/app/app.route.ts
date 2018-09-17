3eb87b8bdd9a6a535212bcb786b27e6343dd7281
root <hlhailan@163.com>
2018-09-17 21:42:41 +0800

    初始化工程

---
+++ src/app/app.route.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { UserService } from 'fccore2';
+import { SigninComponent } from './components/signin/signin.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
+import { LockscreenComponent } from './components/lockscreen/lockscreen.component'; 
+import { LayoutComponent } from './components/layout/layout.component';
+export const AppRoutes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        canActivate: [UserService],
+        children: [
+            {
+                path: 'error',//错误
+                component: ErrorComponent
+            },
+            {
+                path: '',//例子
+                loadChildren: '../feature/fcsample2/index.module#FcsamplesModule'
+            }
+        ]
+    }
+    , {
+        path: 'signin',
+        component: SigninComponent
+    }, {
+        path: 'forgot',//忘记密码
+        component: ForgotComponent,
+    }, {
+        path: 'lockscreen',//锁屏
+        component: LockscreenComponent
+    }
+];
